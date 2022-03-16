cb8dccda388fb496ae8c3b25028369de71b67c00
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2022-03-15 22:20:21 -0300

    Inserção da lista de itens

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
   home: Scaffold(
+    body: Column(
+      children: const <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text("100,00"),
+            subtitle: Text("1000"),
+          ),
+        ),Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text("200,00"),
+            subtitle: Text("1000"),
+          ),
+        ),
+      ],
+    ),
     appBar: AppBar(title: const Text("Transferências"),
     ),
     floatingActionButton: FloatingActionButton(
